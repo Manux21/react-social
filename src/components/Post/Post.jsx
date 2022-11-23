2e695da6d30697f0f794cd90ee5beaf027414966
Manux21 <Manux21@yandex.ru>
2022-11-23 09:59:25 +0300

    added posts component

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './post.scss'
+import {Link} from "react-router-dom";
+import AvatarMike from "../../img/Avatar.png";
 
 const Post = ({post}) => {
   return (
     <div className='post'>
-      <div className='user'></div>
-      <div className='content'></div>
-      <div className='info'></div>
+      <div className="container">
+        <div className='user'>
+          <div className="userInfo">
+            <img src={AvatarMike}alt=""/>
+            <div className="details">
+              <Link to={`/profile/${post.userId}`} style={{textDecoration: 'none', color: "inherit"}}>
+                <span className='name'>{post.name}</span>
+              </Link>
+                <span className='date'>5 minutes ago</span>
+            </div>
+          </div>
+        </div>
+
+        <div className='content'>
+          <p>{post.des}</p>
+          <img src={post.img} alt=""/>
+        </div>
+        <div className='info'></div>
+      </div>
     </div>
   );
 };
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -19,7 +19,7 @@ const Posts = () => {
     {
       id: 2,
       name: 'John Doe',
-      userId: 1,
+      userId: 2,
       avatar: 'https://plus.unsplash.com/premium_photo-1668795180779-2bf36b4b2d6c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
       des: 'Lorem ipsum dolor sit amet, consectetur adip lorem, sed do eiusmod tempor incididunt ut labore lorem. Ut enim ad minim veniam',
       img: 'https://images.unsplash.com/photo-1668934804631-e8337c891e65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
